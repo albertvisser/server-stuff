c95f5d6486bd17ea3389b6124dd4c9aabfb5485b
Albert Visser <albert@lemoncurry>
2015-05-08 11:20:51 +0200

    removed inner functions from django routines; added command to see if all wsgi servers are running

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import shutil
 from fabric.api import * # local, sudo, lcd, hide, settings
 """collection of shortcut functions concerning deployment
@@ -99,6 +100,7 @@ def modconf(*names):
         _modconf(conf.strip())
 
 def _diffconf(name):
+    "compare a configuration file"
     if name in extconf:
         dest, _, fname = extconf[name]
         if fname.startswith('@'):
@@ -138,12 +140,15 @@ def restart_nginx():
     local('sudo killall -HUP nginx')
 
 def start_ftp():
+    "start vsftpd"
     local('sudo start vsftpd')
 
 def stop_ftp():
+    "stop vsftpd"
     local('sudo stop vsftpd')
 
 def restart_ftp():
+    "restart vsftpd"
     local('sudo restart vsftpd')
 
 def stop_php():
@@ -178,19 +183,11 @@ def stop_trac():
     local('sudo kill `cat {}`'.format(trac_pid))
 
 def start_trac():
-    "start local trac server using tracd"
-    ## auth = '{},{},{}'.format(project, os.path.join(TRAC, 'trac_users'), project)
-    ## local('sudo tracd -d -p 9000 --pidfile {} -s {} --basic-auth="{}"'.format(
-        ## trac_pid, TRAC, auth))
-    ## # start = os.path.join(TRAC, 'trac.fcgi')
-    ## # local('sudo spawn-fcgi -f {} -s {} -P {} -u {}'.format(start, trac_sock,
-    ## #     trac_pid, 'www-data'))
-    # trac via gunicorn
+    "start local trac server"
     sock = 'unix:/var/run/lemontrac.sock'
     with lcd(TRAC):
         local('sudo /usr/bin/gunicorn -D -b {} -p {} '
             'tracwsgi:application'.format(sock, trac_pid))
-    ## # local('sudo chown www-data {}'.format(sock))
 
 def restart_trac():
     "restart local trac server"
@@ -204,31 +201,25 @@ def _get_django_args(project):
 
 def stop_django(*project):
     "stop indicated Django server(s)"
-    def stop(project):
-        django_pid = _get_django_args(project)[0]
+    if not project:
+        project = django_project_path.keys()
+    for proj in project:
+        django_pid = _get_django_args(proj)[0]
         if os.path.exists(django_pid):
             local('sudo kill `cat {}`'.format(django_pid))
             local('sudo rm -f {}'.format(django_pid))
-    if not project:
-        project = django_project_path.keys()
-    for proj in project:
-        stop(proj)
 
 def start_django(*project):
     """start indicated Django server(s) using manage.py over fastcgi (python 2)
-    or Gunicorn (python 3)
+    or using Gunicorn (python 3)
     """
-    def start(project):
-        pid, sock, path = _get_django_args(project)
-        with lcd(path):
-            local('sudo /usr/local/bin/gunicorn -D -b unix:{} -p {} '
-                '{}.wsgi:application'.format(sock, pid, project))
-        ## local('sudo chown www-data {}'.format(sock))
-
     if not project:
         project = django_project_path.keys()
     for proj in project:
-        start(proj)
+        pid, sock, path = _get_django_args(proj)
+        with lcd(path):
+            local('sudo /usr/local/bin/gunicorn -D -b unix:{} -p {} '
+                '{}.wsgi:application'.format(sock, pid, proj))
 
 def restart_django(*project):
     "restart django indicated server(s)"
@@ -242,39 +233,43 @@ def restart_django(*project):
         stop_django(proj)
         start_django(proj)
 
+def _fix_media_prefix(path):
+
+    find = 'ADMIN_MEDIA_PREFIX'
+    prefix = "'/static/admin/'"
+    settingsfile = os.path.join(path, 'settings.py')
+    modified = True
+    with open(settingsfile) as _in:
+        buf = _in.read()
+    if find in buf:
+        test = buf.split(find, 1)
+        if len(test) > 1:
+            head = test[0].rstrip().rstrip('#').rstrip() # uncomment if necessary
+            test = test[1].lstrip().lstrip('=').lstrip(' ')
+            if test.startswith(os.linesep):
+                tail = test
+            else:
+                if test.startswith(prefix):
+                   tail = test[len(prefix):]
+                else:
+                    tail = os.linesep + rest
+        else:
+            head = test[0]
+            tail = os.linesep
+    else:
+         head = buf.rstrip()
+         tail = os.linesep
+    if modified:
+        shutil.copyfile(settingsfile, settingsfile + '~')
+        with open(settingsfile, 'w') as _out:
+            _out.write('{}\n{} = {}{}'.format(head, find, prefix,
+                tail))
+
 def django_css(*project):
     "add symlink to admin CSS for Django project"
-    def fix_media_prefix(path):
-        find = 'ADMIN_MEDIA_PREFIX'
-        prefix = "'/static/admin/'"
-        settingsfile = os.path.join(path, 'settings.py')
-        modified = True
-        with open(settingsfile) as _in:
-            buf = _in.read()
-        if find in buf:
-            test = buf.split(find, 1)
-            if len(test) > 1:
-                head = test[0].rstrip().rstrip('#').rstrip() # uncomment if necessary
-                test = test[1].lstrip().lstrip('=').lstrip(' ')
-                if test.startswith(os.linesep):
-                    tail = test
-                else:
-                    if test.startswith(prefix):
-                       tail = test[len(prefix):]
-                    else:
-                        tail = os.linesep + rest
-            else:
-                head = test[0]
-                tail = os.linesep
-        else:
-             head = buf.rstrip()
-             tail = os.linesep
-        if modified:
-            shutil.copyfile(settingsfile, settingsfile + '~')
-            with open(settingsfile, 'w') as _out:
-                _out.write('{}\n{} = {}{}'.format(head, find, prefix,
-                    tail))
-    def link(project):
+    if not project:
+        project = django_project_path.keys()
+    for proj in project:
         pid, sock, path = _get_django_args(project)
         skip = False
         # maak indien nog niet aanwezig directory static onder site root
@@ -290,11 +285,7 @@ def django_css(*project):
         if not skip:
             with lcd(test):
                 local('ln -s /usr/share/pyshared/django/contrib/admin/static/admin')
-            fix_media_prefix(path)
-    if not project:
-        project = django_project_path.keys()
-    for proj in project:
-        link(proj)
+            _fix_media_prefix(path)
 
 def _get_cherry_parms(project=None):
     allproj = ('rst2html', 'logviewer', 'magiokis')
@@ -328,12 +319,11 @@ def start_cherry(*project):
         project = _get_cherry_parms()
     for proj in project:
         conf, pad, prog, pid, _ = _get_cherry_parms(proj)
-        ## with lcd(HERE):
         with lcd(pad):
-            ## # voor Python 2
-            ## local('sudo cherryd -c {} -d -p {} -i {}'.format(conf, pid, prog))
-            # voor Python 3
-            local('sudo /usr/local/bin/cherryd -c {} -d -p {} -i {}'.format(conf,
+            cherrydloc = ''
+            if sys.version >= '3':
+                cherrydloc = '/usr/local/bin/'
+            local('sudo {}cherryd -c {} -d -p {} -i {}'.format(cherrydloc, conf,
                 pid, prog))
 
 def restart_cherry(*project):
@@ -347,6 +337,30 @@ def restart_cherry(*project):
     for proj in project:
         stop_cherry(proj)
         start_cherry(proj)
+
+def check_all_servers(*project):
+    "assuming server is started when there is a pid file"
+    if not project:
+        all_django = list(django_project_path.keys())
+        all_cherry = list(_get_cherry_parms())
+        all_other = ['trac', 'hgweb'] #, 'plone']
+        other_pid = [trac_pid, hgweb_pid] #, plone_pid]
+        project = all_django + all_cherry + all_other
+    all_clear = True
+    for proj in project:
+        if proj in all_django:
+            pid = _get_django_args(proj)[0]
+        elif proj in all_cherry:
+            pid = _get_cherry_parms(proj)[3]
+        elif proj in all_other:
+            pid = other_pid[all_other.index(proj)]
+        if os.path.exists(pid):
+            continue
+        print("{}: no pid file, starting server probably failed".format(
+            proj))
+        all_clear = False
+    if all_clear:
+        print("all local servers ok")
 
 def start_plone():
     "start Plone default instance"
