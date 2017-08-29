a794aa626627a09b34a02554e910e63be1963237
Albert Visser <albert@lemoncurry>
2017-08-29 20:32:36 +0200

    #598 also generate urls for CherryPy apps

--- all_local_pages.py
+++ all_local_pages.py
@@ -35,6 +35,7 @@
 'django.magiokis.nl'                            django
 """
 from __future__ import print_function
+import pdb
 import sys
 import os
 import collections
@@ -50,35 +51,18 @@ check_address = {
         'vertel.magiokis.nl': '/cgi-bin/vertel_select.py',
     },
     'full': {
-        'mydomains.lemoncurry.nl': {
-            # # /home/albert/projects/mydomains./mydomains/urls.py
-            # url(r'^domainchecker/', include('mydomains.index.urls')),
-            # url(r'^$', include('mydomains.index.urls')),
-            #
-            # # Uncomment the admin/doc line below to enable admin documentation:
-            # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-            #
-            # # Uncomment the next line to enable the admin:
-            # url(r'^admin/', include(admin.site.urls)),
-            '/domainchecker/',
-            '/domainchecker/edit/$',
-            '/domainchecker/update/$',
-            '/domainchecker/maintain/$',
-            '/domainchecker/update_list/$',
-        },
-        ## 'myapps.lemoncurry.nl': {
-    ## url(r'^$', include('myapps.index.urls')),
-    ## url(r'^myapps/', include('myapps.index.urls')),
-        ## url(r'^$', 'index', name='home'),
-        ## url(r'^update/$', 'update'),
-        ## url(r'^new/$', 'new'),
-        ## url(r'^add/$', 'add'),
-    ## url(r'^webapps/', include('myapps.webapps.urls')),
-        ## url(r'^$', 'index', name='home'),
-        ## url(r'^update/$', 'update'),
-        ## url(r'^new/$', 'new'),
-        ## url(r'^add/$', 'add'),
-        ## }
+        'muziek.lemoncurry.nl': 'muziek-urls.rst',
+        'absenties.lemoncurry.nl': 'absenties-urls.rst',
+        'doctool.lemoncurry.nl': 'doctool-urls.rst',
+        'mydomains.lemoncurry.nl': 'mydomains-urls.rst',
+        'myapps.lemoncurry.nl': 'myapps-urls.rst',
+        'myprojects.lemoncurry.nl': 'myprojects-urls.rst',
+        'actiereg.lemoncurry.nl': 'actiereg-urls.rst',
+        'albums.lemoncurry.nl': 'albums-urls.rst',
+        'django.magiokis.nl': 'magiokis-django-urls.rst',
+        'cherrypy.magiokis.nl': 'magiokis-cherrypy-urls.rst',
+        'rst2html.lemoncurry.nl': 'rst2html-urls.rst',
+        'logviewer.lemoncurry.nl': 'logviewer-urls.rst',
     },
 }
 
@@ -201,11 +185,12 @@ def build_urllist(project, result):
                 ready = newready
         analyzed.extend(ready)
     result = analyzed
-    # domein  voorvoegen
-    if project == 'magiokis-django':
-        result = ['django.magiokis.nl/{}'.format(x) for x in analyzed]
-    else:
-        result = ['{}.lemoncurry.nl/{}'.format(project, x) for x in analyzed]
+    # domein  voorvoegen - alleen / is voldoende
+    ## if project == 'magiokis-django':
+        ## result = ['django.magiokis.nl/{}'.format(x) for x in analyzed]
+    ## else:
+        ## result = ['{}.lemoncurry.nl/{}'.format(project, x) for x in analyzed]
+    result = ['/{}'.format(x) for x in analyzed]
     return result
 
 
@@ -218,6 +203,260 @@ def discover_django_urls(project):
     ## return discover(root)
     ## return parse_result(discover(root))
     return build_urllist(project, parse_result(discover(root)))
+
+
+def do_logviewer():
+    """
+    introspectie is waarschijnlijk netter, maar eerst maar even plat lezen:
+    """
+    data = []
+    with open('/home/albert/projects/logviewer/viewlogs_cherry.py') as _in:
+        data = _in.readlines()
+    exposed = False
+    views = []
+    for line in data:
+        if exposed:
+            views.append(line.strip())
+            exposed = False
+        elif '@cherrypy.expose' in line:
+            exposed = True
+    urls = []
+    for line in [x.replace('def ', '').replace('self', '') for x in views]:
+        if 'index' in line:
+            url = '/'
+        elif '()' in line:
+            urls.append('/' + line.replace('():', '/'))
+            continue
+        else:
+            url = '/' + line[:line.index('(')].strip() + '/'
+        needed, optional = [], []
+        for param in line[line.index('(') + 1:line.index(')')].split(','):
+            param = param.strip()
+            if '=' in param:
+                optional.append(param[:param.index('=')])
+            elif param:
+                needed.append(param)
+        url2 = ''
+        if needed:
+            url += '?' + '=<>&'.join(needed) + '=<>'
+            url2 = url + '=<>&'.join(optional) + '=<>'
+        elif optional:
+            url2 = url + '?' + '=<>&'.join(optional) + '=<>'
+        urls.append(url)
+        if url2:
+            urls.append(url2)
+    return urls
+
+
+def do_rst2html():
+    """
+    de views kunnen allemaal dezelfde set argumenten verwerken; aan de functie
+    die wordt aangeroepen kun je zien welke argumenten werkelijk vereist zijn
+    """
+    data = []
+    with open('/home/albert/projects/rst2html/rst2html.py') as _in:
+        data = _in.readlines()
+    exposed = False
+    urls = []
+    for line in data:
+        line = line.strip()
+        if line.startswith('#'):
+            continue
+        if exposed:
+            if 'def' in line:
+                view = line  # we only need the function name, so one line is enough
+                url = '/' + view[:view.index('(')].replace('def ', '') + '/'
+                if url == '/index/':
+                    url = '/'
+                body = []
+                in_docstring = False
+            elif 'return' in line:
+                exposed = False
+                # look at called function(s) - calls are immediately relayed so we can use
+                # their arguments
+                body = ''.join(body)
+                params = body[body.index('(') + 1:body.index(')')]
+                if params:
+                    args = [x.strip() for x in params.split(',')]
+                    url += '?' + '=<>&'.join(args) + '=<>'
+                urls.append(url)
+            else:
+                if not in_docstring and line.startswith('"""'):
+                    in_docstring = True
+                    line = line[3:]
+                if in_docstring:
+                    if line.endswith('"""'):
+                        in_docstring = False
+                    continue
+                try:
+                    line = line[line.index('#'):]
+                except ValueError:
+                    pass
+                body.append(line)
+        elif '@cherrypy.expose' in line:
+            exposed = True
+            ## pdb.set_trace()
+    return urls
+
+
+def do_magiokis():
+    """
+    er worden attributen aangemaakt voor de secties bv. self.ow = OldWhoresPage()
+    dwz dat is het eerste onderdeel van de url bv /ow/
+    bij SpeelMee wordt dat trucje herhaald
+    de naam van een functie is bepalend voor het vervolgdeel van de url
+    met dien verstande dat index de / locatie is en default ook wordt ingeslikt
+    (maar dan is er sprake van extra parameters)
+    """
+    def getnamestuff(name):
+        name == test[1].rstrip(':')
+        needed, optional = [], []
+        if '(' in name:
+            ix1 = name.index('(')
+            ix2 = name.index(')')
+            params = name[ix1 + 1:ix2].split(',')
+            name = name[:ix1]
+            for parm in params:
+                parm = parm.strip()
+                if parm == 'self':
+                    continue
+                if '=' in parm:
+                    name2, val = [x.strip() for x in parm.split('=')]
+                    name2 = '<{}>'.format(name2)
+                    if val != "''":
+                        name2 += val.strip("'")
+                    optional.append(name2)
+                else:
+                    needed.append('<{}>'.format(parm))
+        return name, needed, optional
+
+
+    urls = []
+    data = []
+    with open('/home/albert/projects/magiokis-cherry/magiokis.py') as _in:
+        data = _in.readlines()
+    # lees class
+    # als __init__ methode dan (sub)secties
+    # index is /
+    # default verdient verder analyseren
+    # andere naam is pagina adres
+    exposed = in_init = in_docstring = False
+    sections = collections.defaultdict(dict)
+    urls = collections.defaultdict(list)
+    classname = ''
+    for line in data:
+        line = line.strip()
+        if not line:
+            if in_init:
+                for line in body:
+                    if line.startswith('self.'):
+                        test = line.replace('self.', '').split(' = ')
+                        sections[classname][test[1].replace('()', '')] = test[0]
+                in_init = False
+            continue
+        if line.startswith('#'):
+            continue
+        if line == '@cherrypy.expose':
+            exposed = True
+            continue
+        test = line.strip().split(None, 1)
+        if test[0] == 'class':
+            classname = getnamestuff(test[1])[0]
+            ## if classname == 'HomePage':
+                ## urlbase = '/'
+        elif not classname:
+            continue
+        elif test[0] == 'def':
+            viewname, args, opts = getnamestuff(test[1])  # assumes function call only spans one line
+            if viewname == '__init__':
+                in_init = True
+                body = []
+            elif exposed:
+                if args or opts:
+                    print('found def:', classname, viewname)
+                    print('args:', args)
+                    print('opts:', opts)
+                if viewname == 'index':
+                    urls[classname].append('/')
+                    urlbasename = ''
+                elif viewname == 'default':
+                    urlbasename = ' '
+                else:
+                    urlbasename = '/' + viewname
+                if urlbasename:
+                    if args:
+                        urlbasename += '/' + '/'.join(args)
+                    urllist = [urlbasename.strip()]
+                    for item in opts:
+                        if item.endswith('>'):
+                            ## print(urllist, item)
+                            urllist = ['/'.join((x, item)) for x in urllist]
+                        else:
+                            dflt = item[item.index('>') + 1:]
+                            urllist1 = ['/'.join((x, dflt)) for x in urllist]
+                            item = item.replace(dflt, '')
+                            urllist2 = ['/'.join((x, item)) for x in urllist]
+                            urllist = urllist1 + urllist2
+                    ## if opts:
+                        urls[classname].extend(urllist)
+                    urls[classname].append(urlbasename.strip())
+                in_docstring = False
+        elif test[0] == 'return':
+            ## if in_init:
+                ## for line in body:
+                    ## if line.startswith('self.'):
+                        ## test = line.replace('self.', '').split(' = ')
+                        ## sections[classname][test[1].replace('()', '')] = test[0]
+            ## in_init = False
+            exposed = False
+        elif in_init:
+            if not in_docstring and line.startswith('"""'):
+                in_docstring = True
+                line = line[3:]
+            if in_docstring:
+                if line.endswith('"""'):
+                    in_docstring = False
+                continue
+            try:
+                line = line[line.index('#'):]
+            except ValueError:
+                pass
+            body.append(line)
+    homepagekey = 'HomePage:'
+    for key in sections:
+        if key == homepagekey:
+            continue
+        for item, value in sections[key].items():
+            prefix = sections[homepagekey][key]
+            sections[homepagekey][item] = '/'.join((prefix, value))
+    final = []
+    for section, urllist in urls.items():
+        if section == homepagekey:
+            continue
+        newlist = [sections[homepagekey][section].join(('/', x)) for x in urllist]
+        for ix, item in enumerate(newlist):
+            if not item.endswith('/'):
+                newlist[ix] += '/'
+        final.extend(newlist)
+    final.extend(urls[homepagekey])
+    return sorted(final)
+
+
+def discover_cherrypy_urls(project):
+    """om te beginnen even een opsomming van wat ik aantref in de diverse scripts
+    en hoe daarmee om te gaan:
+    - bepaal class
+    - selecteer methoden gedecoreerd met @cherrypy.expose
+    - hoe link je een view aan een url?
+    - anders dan bij django werken deze allemaal verschillend
+    """
+    if project == 'logviewer':
+        result = do_logviewer()
+    elif project == 'magiokis-cherry':
+        result = do_magiokis()
+    elif project == 'rst2html':
+        result = do_rst2html()
+    return result
 
 
 if __name__ == '__main__':
@@ -233,7 +472,8 @@ if __name__ == '__main__':
     ## print(parse_part('\d+'))
     ## print(parse_part('(hello|sailor)'))
     project = sys.argv[1]
-    data = discover_django_urls(project)
+    ## data = discover_django_urls(project)
+    data = discover_cherrypy_urls(project)
     with open(project + '-urls.rst', 'w') as _out:
         for x in data:
             print(x, file=_out)
