7c8ae76fa3708cc5ddeb5b7b12dae82cf0a66352
Albert Visser <albert.visser@gmail.com>
2021-09-27 20:39:08 +0200

    add frozen repos with gitweb add-repo

--- tasks_gitweb.py
+++ tasks_gitweb.py
@@ -8,11 +8,15 @@ from config import HOME, HGWEB, runpath
 from tasks_shared import report_result, remove_result
 
 
-@task(help={'names': 'comma separated list of repository names (no spaces)'})
-def addrepo(c, names):
+@task(help={'names': 'comma separated list of repository names (no spaces)',
+            'frozen': 'add repo(s) that is moved to the frozen section'})
+def addrepo(c, names, frozen=False):
     "add specified repos so that they can be viewed with gitweb"
     for name in names.split(','):
         with c.cd('/var/lib/git'):
-            c.run('sudo ln /home/albert/git_repos/{0}/.git {0}.git'.format(name))
+            if frozen:
+                c.run('sudo ln -s /home/albert/git_repos/.frozen/{0}/.git {0}.git'.format(name))
+            else:
+                c.run('sudo ln -s /home/albert/git_repos/{0}/.git {0}.git'.format(name))
 
 
