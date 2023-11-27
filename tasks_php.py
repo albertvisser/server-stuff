10470b7af2d685e767f0dfa21e252e927dafcd6e
Albert Visser <albert.visser@gmail.com>
2023-11-27 18:40:42 +0100

    correcte servicenaam gebruiken

--- tasks_php.py
+++ tasks_php.py
@@ -2,25 +2,20 @@
 """
 from invoke import task
 
-PHPX = 'php7.4-fpm'
-
 
 @task
 def stop(c):
     "stop php"
-    # c.run('sudo {}/{} stop'.format(INIT, PHPX))
-    c.run('sudo systemctl stop php-fpm7.service')
+    c.run('sudo systemctl stop php-fpm.service')
 
 
 @task
 def start(c):
     "start php"
-    # c.run('sudo {}/{} start'.format(INIT, PHPX))
-    c.run('sudo systemctl start php-fpm7.service')
+    c.run('sudo systemctl start php-fpm.service')
 
 
 @task
 def restart(c):
     "restart php"
-    # c.run('sudo {}/{} restart'.format(INIT, PHPX))
-    c.run('sudo systemctl restart php-fpm7.service')
+    c.run('sudo systemctl restart php-fpm.service')
